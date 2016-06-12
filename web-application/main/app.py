435665e774e7274fc39132fe979f5b2671a82039
Isabek <isabek2309@gmail.com>
2016-06-12 04:47:17 +0300

    - Installed Flask-Migrate module.

--- web-application/main/app.py
+++ web-application/main/app.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
 from base import Base
-from main.extensions import db
+from main.extensions import db, migrate
 
 
 class App(Base):
     def configure_database(self):
         db.app = self
         db.init_app(self)
+
+    def configure_extensions(self):
+        migrate.init_app(self, db)
--- web-application/manage.py
+++ web-application/manage.py
@@ -5,6 +5,7 @@ from flask_script import Manager
 from main import config
 from main.app import App
 from frontend.views import frontend
+from flask_migrate import MigrateCommand
 
 
 def app_factory(conf, app_name):
@@ -18,6 +19,7 @@ def app_factory(conf, app_name):
 
 if __name__ == '__main__':
     manager = Manager(app_factory)
+    manager.add_command('db', MigrateCommand)
     manager.add_option("-n", "--name", dest="app_name", required=False, default=config.Config.PROJECT)
     manager.add_option("-c", "--config", dest="conf", required=False, default=config.DevelopConfig)
     manager.run()
