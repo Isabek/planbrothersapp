acf8b8c4fe32f20bdac2863a13f37bb38ec8c4c2
Isabek <isabek2309@gmail.com>
2016-06-15 18:09:36 +0300

    - Updated default host to 0.0.0.0

--- web-application/manage.py
+++ web-application/manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-from flask_script import Manager
+from flask_script import Manager, Server
 
 from main.settings import config
 from main.app import App
@@ -21,6 +21,7 @@ def app_factory(environment, app_name):
 if __name__ == '__main__':
     manager = Manager(app_factory)
     manager.add_command('db', MigrateCommand)
+    manager.add_command('runserver', Server(host='0.0.0.0'))
     manager.add_option("-n", "--name", dest="app_name", required=False, default='default')
     manager.add_option("-e", "--environment", dest="environment", required=False, default='development')
     manager.run()
